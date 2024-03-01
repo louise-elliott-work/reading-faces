5a2855bc3f68dc90f3731ccec8b77855889bf90e
Louise Elliott <louise.elliott.work@gmail.com>
2024-03-01 17:36:22 +0000

    files added from initial design

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span className="brand-tagline-1">emotional literacy</span>
+            <span className="brand-tagline-separator"> - </span>
+            <span className="brand-tagline-2">learning through play</span>
+        </div>
+    )
+};
+
+export default Footer;
