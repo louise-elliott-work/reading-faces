0f16733306b41c6c037bfc77f4bc21e175003974
Louise Elliott <louise.elliott.work@gmail.com>
2024-03-04 10:57:58 +0000

    image file paths corrected

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -10,11 +10,11 @@ const Bio = () => {
                 <h1>Contact</h1>
                 <p>summary info here</p>
                 <p>Louise Elliott - March 2024</p>
-                <div className="headshot-container"><img className="headshot" src="public/headshot.jpg" alt={`Headshot of Louise Elliott`} /></div>
+                <div className="headshot-container"><img className="headshot" src="/headshot.jpg" alt={`Headshot of Louise Elliott`} /></div>
                 <div className="links">
-                    <Link to="https://github.com/louise-elliott-work" target="_blank"><img className="github" src="/public/github.svg"/></Link>
-                    <Link to="https://www.linkedin.com/in/louise-elliott-work/" target="_blank"><img className="linkedin" src="/public/linkedin.svg"/></Link>
-                    <Link to="louise.elliott.work@gmail.com" target="_blank"><img className="envelope" src="/public/envelope.svg"/></Link>                             
+                    <Link to="https://github.com/louise-elliott-work" target="_blank"><img className="github" src="/github.svg"/></Link>
+                    <Link to="https://www.linkedin.com/in/louise-elliott-work/" target="_blank"><img className="linkedin" src="/linkedin.svg"/></Link>
+                    <Link to="louise.elliott.work@gmail.com" target="_blank"><img className="envelope" src="/envelope.svg"/></Link>                             
                 </div>
             </div>
         </>
