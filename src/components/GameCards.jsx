503b5b00ddf5500a5a5912e89cd63847c7eb24db
Louise Elliott <louise.elliott.work@gmail.com>
2024-03-09 15:59:13 +0000

    card shuffle function added in

--- src/components/GameCards.jsx
+++ src/components/GameCards.jsx
@@ -4,6 +4,7 @@ import './GameCards.css';
 
 const GameCards = () => {
 
+    // TODO Amend so clicking a card, turns over that card only
     const useToggle = (initialState = true) => {
         const [state, setState] = useState(initialState);
         const toggle = useCallback(()=> setState((state) => !state), []);
@@ -11,22 +12,37 @@ const GameCards = () => {
     }
     const [toggle, setToggle] = useToggle();
 
+    // * Fisher-Yates sorting algorithm used for card shuffling
+    const shuffleCards = (reference) => { 
+        let currentIndex = reference.length,  randomIndex;
+        while (currentIndex > 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [reference[currentIndex], reference[randomIndex]] = [reference[randomIndex], reference[currentIndex]];
+        }
+        return reference;
+    }
+    // TODO Amend so card order is randomised each time the game ends of the page loads
+    shuffleCards(reference);
+    console.log(reference);
+
     return (
         <>
             <p>Click on a card to turn it over and find the pairs</p>
+
             <div className="cards-grid">
                     {reference.map((emotion) => toggle && (
                         <div key={emotion.id}>
-                            <div>
                                 <button className="single-card" key={emotion.image} onClick={setToggle}>
-                                    <div className="image-container single-card-turned"><img className= "emotion-image" src={emotion.image} alt={`Photo of ${emotion.name}`} /></div>
+                                    <div className="single-card-turned"><img className="image" src={emotion.image} alt={`Photo of ${emotion.name}`} /></div>
                                 </button>
-                            </div>
-                            <div>
+                        </div>
+                    ))}
+                    {reference.map((emotion) => toggle && (
+                        <div key={emotion.id}>
                                 <button className="single-card" key={emotion.name} onClick={setToggle}>
-                                    <p className="emotion-name single-card-turned">{emotion.name}</p>
+                                    <div className="single-card-turned">{emotion.name}</div>
                                 </button>
-                            </div>
                         </div>
                     ))}
             </div>
