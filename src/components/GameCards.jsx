b3da6e2a0ec657328e04c85ea377ef76121eab5e
Louise Elliott <louise.elliott.work@gmail.com>
2024-03-20 19:28:47 +0000

    shuffle function working on emotion images from json and displaying on cards

--- src/components/GameCards.jsx
+++ src/components/GameCards.jsx
@@ -4,7 +4,6 @@ import './GameCards.css';
 
 const GameCards = () => {
 
-    const newTestArray = [];
     // TODO Amend so clicking a card, turns over that card only
     const [cardState, setCardState] = useState(false);
     const handleClick= () => {
@@ -15,86 +14,61 @@ const GameCards = () => {
     // * Assign relevant class name to toggle card between active and inactive
     let stateCheck = cardState ? 'active-card' : 'inactive-card';
 
-    //! Test new array of names and images combined
-    const createNewTestArray = (newTestArray) => { 
-        let nameArrayTest = ['happy', 'confused', 'sad'];
-        let imageArrayTest = ['test1', 'test2', 'test3'];
-        newTestArray = nameArrayTest.concat(imageArrayTest);
-        // * Fisher-Yates sorting algorithm used for card shuffling
-        const shuffleTestCards = (newTestArray) => { 
-            let currentIndex = newTestArray.length,  randomIndex;
-            while (currentIndex > 0) {
-                randomIndex = Math.floor(Math.random() * currentIndex);
-                currentIndex--;
-                [newTestArray[currentIndex], newTestArray[randomIndex]] = [newTestArray[randomIndex], newTestArray[currentIndex]];
-            }
+    // TODO correct this so it is a loop rather than manually searching by index reference
+    let namesOnly;
+    const namesOnlyArray = () => {
+        namesOnly = [reference[0].name, reference[1].name, reference[2].name, reference[3].name, reference[4].name, reference[5].name, reference[6].name, reference[7].name, reference[8].name, reference[9].name];
+        return(namesOnly)
+    }
+    namesOnlyArray(namesOnly);
+    // * Fisher-Yates sorting algorithm for card shuffling
+    const namesOnlyShuffled = (namesOnly) => { 
+        let currentIndex = namesOnly.length,  randomIndex;
+        while (currentIndex > 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [namesOnly[currentIndex], namesOnly[randomIndex]] = [namesOnly[randomIndex], namesOnly[currentIndex]];
         }
-        shuffleTestCards(newTestArray);
-    // * Shuffle function working fine for test array, as seen in console log, changing when clicked/refreshed for now.
-    console.log(newTestArray);
-    return newTestArray;
     }
-    createNewTestArray(newTestArray);
+    namesOnlyShuffled(namesOnly);
 
-// TODO this would need to be the names of the emotions having looped through the json reference file
-let namesOnly;
-const namesOnlyArray = () => {
-    namesOnly = [reference[0].name, reference[1].name, reference[2].name, reference[3].name, reference[4].name, reference[5].name];
-    console.log(namesOnly);
-    return(namesOnly)
-}
-namesOnlyArray(namesOnly);
-
-const basicTestArray = namesOnly;
-console.log(basicTestArray);
-
-// * Fisher-Yates sorting algorithm used for card shuffling
-const basicTestArrayShuffled = (basicTestArray) => { 
-    let currentIndex = basicTestArray.length,  randomIndex;
-    while (currentIndex > 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-        [basicTestArray[currentIndex], basicTestArray[randomIndex]] = [basicTestArray[randomIndex], basicTestArray[currentIndex]];
+    // TODO correct this so it is a loop rather than manually searching by index reference
+    let imagesOnly;
+    const imagesOnlyArray = () => {
+        imagesOnly = [reference[0].image, reference[1].image, reference[2].image, reference[3].image, reference[4].image, reference[5].image, reference[6].image, reference[7].image, reference[8].image, reference[9].image];
+        return(imagesOnly)
     }
-}
-basicTestArrayShuffled(basicTestArray);
-console.log(basicTestArray);
+    imagesOnlyArray(imagesOnly);
+    // * Fisher-Yates sorting algorithm for card shuffling
+    const imagesOnlyShuffled = (imagesOnly) => { 
+        let currentIndex = imagesOnly.length,  randomIndex;
+        while (currentIndex > 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [imagesOnly[currentIndex], imagesOnly[randomIndex]] = [imagesOnly[randomIndex], imagesOnly[currentIndex]];
+        }
+    }
+    imagesOnlyShuffled(imagesOnly);
 
+    return (
+        <>
+            <p className='instruction'>Click on a card to turn it over and find the pairs</p>
+            <div className="cards-grid">
+                {namesOnly.map((emotion, index) => (
+                    <button key={index} className="single-card text" onClick={handleClick}>
+                        <div className={`${stateCheck}`}><p>{emotion}</p></div>
+                    </button>
+                ))}
+                {imagesOnly.map((emotion, index) => (
+                    <button key={index} className="single-card" onClick={handleClick}>
+                        <div className={`${stateCheck}`}><img className="image" src={emotion} alt={`Photo of ${emotion}`}></img></div>
+                    </button>
+                ))}
 
-        return (
-            <>
-                <p className='instruction'>Click on a card to turn it over and find the pairs</p>
-                <div className="cards-grid">
-                    {basicTestArray.map((emotion, index) => (
-                        <button key={index} className="single-card" onClick={handleClick}>
-                            <div className={`${stateCheck}`}>{emotion}</div>
-                        </button>
-                    ))}
-                </div>
-            </>
-        )
+            </div>
+        </>
+    )
 
-    // return (
-    //     <>
-    //         <p className='instruction'>Click on a card to turn it over and find the pairs</p>
-    //         <div className="cards-grid">
-    //                 {reference.map((emotion) => (
-    //                     <div key={emotion.id}>
-    //                             <button className="single-card" key={emotion.image} onClick={handleClick}>
-    //                                 <div className={`${stateCheck}`}><img className="image" src={emotion.image} alt={`Photo of ${emotion.name}`} /></div>
-    //                             </button>
-    //                     </div>
-    //                 ))}
-    //                 {reference.map((emotion) => (
-    //                     <div key={emotion.id}>
-    //                             <button className="single-card" key={emotion.name} onClick={handleClick}>
-    //                                 <div className={`${stateCheck}`}>{emotion.name}</div>
-    //                             </button>
-    //                     </div>
-    //                 ))}
-    //         </div>
-    //     </>
-    // )
 };
 
 export default GameCards;
